0a99a4b056f0449307096845d6de95ea577a8af3
tsamantanis <philip.tsamantanis@icloud.com>
2021-11-22 14:22:55 -0800

    Add current and next location banners

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const current = 'GR'
+const current = {
+    city: 'Athens',
+    code: 'GR'
+}
+
+const next = {
+    city: 'Cape Town',
+    code: 'ZA',
+    date: 'November 24, 2021'
+}
+
 const visited = [
     'BR',
     'TH',
@@ -16,14 +26,30 @@ const visited = [
     'CH',
     'GB',
     'US',
-    'AE'
+    'AE',
+    'ZA'
 ]
+
+async function loadItenarary() {
+    document.getElementById('current-location').innerHTML = current.city + ", " + current.code;
+    document.getElementById('next-location').innerHTML = next.city + ", " + next.code;
+    document.getElementById('next-date').innerHTML = "on " + next.date;
+    const locations = document.getElementsByClassName('location-container')
+    for (let i = 0; i < locations.length; i += 1) {
+        console.log(locations[i].children)
+        if (locations[i].children['current-location']) {
+            locations[i].classList.add('bg-current')
+        } else if (locations[i].children['next-location']) {
+            locations[i].classList.add('bg-next')
+        }
+    }
+}
+
 async function loadGlobe() {
     const countries_data = await fetch('./globe_data.geojson')
     const countries = await countries_data.json()
-    console.log(countries)
     const colors = countries.features.map(country => {
-        if (current === country.properties['ISO_A2']) return '#FE5F55'
+        if (current.code === country.properties['ISO_A2']) return '#FE5F55'
         if (visited.includes(country.properties['ISO_A2'])) return '#643CF5'
         return '#262626'
         
@@ -33,7 +59,7 @@ async function loadGlobe() {
     let colorCount = 0;
     const world = Globe({ animateIn: true })
         .backgroundColor("#ffffff")
-        
+        .height(window.innerHeight * 0.734)
         .globeImageUrl('./gradient.png')
         .hexPolygonsData(countries.features)
         .hexPolygonResolution(3)
@@ -46,4 +72,5 @@ async function loadGlobe() {
     world.controls().enableZoom = false
 }
 
+loadItenarary()
 loadGlobe()
